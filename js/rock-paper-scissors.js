a33887eb1f1e0224266e84cc0bd70857fd746f44
JoshuaHolloway <jhollow6@asu.edu>
2020-08-11 18:12:51 -0500

    added (whack) UI

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -13,9 +13,15 @@ const randomly_select_play = () => {
 
 const play_game = (player_choice) => {
     
+    // Apply advanced artificial-intelligence:
     const ai_choice = randomly_select_play();
-    console.log(`Player=${player_choice} and AI=${ai_choice}!`);
-
+    
+    // Grab the 
+    const choices_elem = document.querySelector('#choices');
+    const result_elem = document.querySelector('#result');
+    
+    // Display the chosen plays to the screen
+    choices_elem.innerHTML = `Player=${player_choice} and AI=${ai_choice}!`;
 
     // Nine total outcomes:
     // Seven if all ties are equal:
@@ -42,22 +48,36 @@ const play_game = (player_choice) => {
     
     //  P,R -> P1   AI
     //  R,P -> P2
-    else if (ai_choice==='P' && player_choice==='R') //  P,R -> P1 (AI)
-        console.log(`AI-wins: Paper smothers rock!`);
-    else if (ai_choice==='R' && player_choice==='P') //  R,P -> P2
-        console.log(`You-win: Paper smothers rock!`);
+    else if (ai_choice==='P' && player_choice==='R') { //  P,R -> P1 (AI)
+        choices_elem.innerHTML = 'Player chose Rock and AI choce Paper.';
+        result_elem.innerHTML = `AI-wins: Paper smothers rock!`;
+    }
+    else if (ai_choice==='R' && player_choice==='P') { //  R,P -> P2
+        choices_elem.innerHTML = 'Player chose Paper and AI choce Rock.';
+        result_elem.innerHTML = 'You-win: Paper smothers rock!';
+    }
 
     //  S,P -> P1   AI
     //  P,S -> P2
-    else if (ai_choice==='S' && player_choice==='P') //  S,P -> P1   AI
-        console.log(`AI-wins: Scissors cut paper!`);
-    else if (ai_choice==='P' && player_choice==='S') //  P,S -> P2
-        console.log(`You-win: Scissors cut paper!`);
-
+    else if (ai_choice==='S' && player_choice==='P') { //  S,P -> P1   AI
+        choices_elem.innerHTML = 'Player chose Paper and AI chose Scissor.';
+        result_elem.innerHTML = `AI-win: Scissors cut paper!`;
+    }
+    else if (ai_choice==='P' && player_choice==='S') { //  P,S -> P2
+        choices_elem.innerHTML = 'Player chose scissors and AI chose Paper';
+        result_elem.innerHTML = 'You-win: Scissors cut paper!';
+    }
     //  R,S -> P1   AI
     //  S,R -> P2
-    else if (ai_choice==='R' && player_choice==='S') //  R,S -> P1   AI
-        console.log(`AI-wins: Rock smashes scissors!`);
-    else if (ai_choice==='S' && player_choice==='R') //  S,R -> P2
-        console.log(`You-win: Rock smashes scissors!`);
+    else if (ai_choice==='R' && player_choice==='S') { //  R,S -> P1   AI
+        choices_elem.innerHTML = 'Player chose scissors and AI chose Rock';
+        result_elem.innerHTML = 'AI-wins: Rock smashes scissors!';
+    }
+    else if (ai_choice==='S' && player_choice==='R') { //  S,R -> P2
+        choices_elem.innerHTML = 'Player chose Rock and AI chose Scissors';
+        result_elem.innerHTML = 'You-win: Rock smashes scissors!';
+    } else {
+        choices_elem.innerHTML = 'You entered an invalid choice!!!';
+        result_elem.innerHTML = 'Please enter P, R, or S';
+    }
 };
